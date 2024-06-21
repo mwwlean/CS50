7fed06ffedd72de2802f9bbaae69cf6883ba5e5e
mwwlean <foolsparadise984@gmail.com>
2024-06-21 15:51:50 +0800

    Problem Set 1 - Deep

---
+++ Python/Problem Set 1/deep.py
@@ -0,0 +1,6 @@
+user = input("What is the Answer to the Great Question of Life,  the Universe, and Everything? ").strip().lower()
+
+if user == "42" or user == "forty-two" or user == "forty two":
+    print("Yes")
+else:
+    print("No")
